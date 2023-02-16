7a689ccd486ac7f5020396618fb3e391f00bfb04
FLANDIN Ghislain <gflandin@wideagency.com>
2023-02-16 16:58:31 +0100

    Initial commit

---
+++ src/locations/Field.tsx
@@ -0,0 +1,71 @@
+import React, {useEffect, useState} from 'react';
+import { FieldExtensionSDK } from '@contentful/app-sdk';
+import {useCMA, useSDK} from '@contentful/react-apps-toolkit';
+import { Select } from '@contentful/f36-components';
+
+const Field = () => {
+  const [entries, setEntries] = useState([])
+  const [selectValue, setSelectValue] = useState('')
+  const [defaultLocale, setDefaultLocale] = useState('')
+  const sdk = useSDK<FieldExtensionSDK>();
+  const cma = useCMA();
+
+  useEffect( () => {
+    sdk.window.startAutoResizer();
+    setDefaultLocale(sdk.locales.default)
+
+    const linkContentTypes = sdk.field.validations[0].linkContentType
+    const entityType = (linkContentTypes) ? linkContentTypes[0] : ''
+
+    // get current field value if exists
+    if(sdk.field.getValue()) {
+      Promise.resolve(cma.entry.get({
+        entryId: sdk.field.getValue().sys.id
+      })).then(data => {
+        setSelectValue(data.fields.value[sdk.locales.default])
+      })
+    }
+
+    Promise.resolve(cma.entry.getPublished({
+      query: {
+        content_type: entityType,
+        order: 'fields.label'
+      }
+    }))
+        .then((data:any) => {
+          setEntries(data.items)
+        })
+    return () => {
+      sdk.window.stopAutoResizer();
+    };
+
+  }, [cma.entry, sdk.editor, sdk.field, sdk.field.validations, sdk.locales.default, sdk.window])
+
+
+  const handleOnChange = (event:any) => {
+    setSelectValue(event.target.value)
+    const selectedEntry: any = entries.find((entry:any) => entry.fields.value[defaultLocale] === event.target.value)
+    const reference = {
+      sys: {
+        id: selectedEntry.sys.id,
+        linkType: "Entry",
+        type: "Link"
+      }
+    }
+    sdk.field.setValue(reference).then(data => {
+      console.log('selected')
+    })
+  }
+
+  return (
+      <div>
+        <Select id={sdk.field.id} name={sdk.field.id} value={selectValue} onChange={handleOnChange}>
+          {entries.map((entry:any) => (
+              <Select.Option key={entry.sys.id} value={entry.fields.value[defaultLocale]}>{entry.fields.label[sdk.field.locale] ? entry.fields.label[sdk.field.locale] : entry.fields.label[defaultLocale]}</Select.Option>
+          ))}
+        </Select>
+      </div>
+  )
+};
+
+export default Field;
